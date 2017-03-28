10ad824b069ef482aaf0f7289be9a369401af261
hung135 <hung135@hotmail.com>
2017-03-27 22:14:52 -0400

    add updated version of agent

--- install_gocd.sh
+++ install_gocd.sh
@@ -1,5 +1,4 @@
  docker pull gocd/gocd-server:16.9.0
-      docker pull gocd/gocd-agent
       #https://hub.docker.com/r/microsoft/mssql-server-linux/
       #https://hub.docker.com/r/gocd/gocd-server/
       docker run --restart=always -v /etc/ssl/certs:/etc/ssl/certs:ro --name gocdserver -i -t -d -p 8153:8153 -p 8154:8154 gocd/gocd-server:16.9.0
@@ -11,5 +10,5 @@
 
       #https://hub.docker.com/r/gocd/gocd-agent/
       #this has ben deprecated and they removed the repo
-      #docker run --restart=always --name gocd-agent1 -it -d -e GO_SERVER=192.168.33.10 gocd/gocd-agent
-      #docker run --restart=always --name gocd-agent2 -it -d -e GO_SERVER=192.168.33.10 gocd/gocd-agent
+      docker run --restart=always --name gocd-agent1 -it -d -e GO_SERVER_URL=192.168.33.10 gocd/gocd-agent-ubuntu-16.04:v17.3.0
+      docker run --restart=always --name gocd-agent2 -it -d -e GO_SERVER_URL=192.168.33.10 gocd/gocd-agent-ubuntu-16.04:v17.3.0
